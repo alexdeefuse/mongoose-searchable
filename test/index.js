69518e62aaea2afc6412d6b94dc4d38d0eaec9ef
Alex Ghiu <alex@deefuse.com>
2012-04-25 22:03:08 +0300

    module setup done

---
+++ test/index.js
@@ -0,0 +1,10 @@
+var should		= require('should');
+
+var mongoose	= require('mongoose'),
+	Schema		= mongoose.Schema;
+	
+var searchable	= require('../');
+
+describe('searchable', function(){
+	
+});
