626c0df45382c81eab0e8426d3b06671a4612491
Alex Ghiu <alex@deefuse.com>
2012-04-25 22:51:41 +0300

    tested the presence of hook for mongoose

--- test/index.js
+++ test/index.js
@@ -3,8 +3,68 @@ var should		= require('should');
 var mongoose	= require('mongoose'),
 	Schema		= mongoose.Schema;
 	
-var searchable	= require('../');
+var Searchable	= require('../');
+
+/**
+ *	setup
+ */
+
+Searchable.setup( mongoose );
+
+mongoose.connect('localhost', 'mongoose_searchable');
+
+/**
+ *	schemas
+ */
+
+/**
+ *	testing
+ */
 
 describe('searchable', function(){
+	
+	before(function(done){
+		mongoose.connection.on('open', done);
+	});
+	
+	it('should have a setup to link to mongoose', function(done){
+		Searchable.should.have.property('setup');
+		done();
+	});
+	
+	it('should have a collection name property', function(done){
+		Searchable.should.have.property('collection_name');
+		done();
+	});
+	
+	it('should create its own collection', function(done){
+		var c = 0;
+		try{
+			mongoose.model(Searchable.collection_name);
+		}catch(e){
+			c++;
+		}
+		c.should.equal(0);
+		done();
+	});
+	
+	it('should add expose a plugin to be added to other schemas', function(done){
+		Searchable.should.have.property('hook');
+		done();
+	});
+	
+	it('should copy keywords from linked collections', function(done){
+		
+	});
+	
+	it('should allow each linked collection to be searched independently');
+	
+	it('should allow global searching on all linked collections');
+	
+	it('should allow to restrict the number of collections to be searched');
+	
+	it('should allow sorting on returned results');
+	
+	it('should allow the returning of diffrent types of documents');
 	
 });
